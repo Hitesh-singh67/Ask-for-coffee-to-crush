f8a28e07f7f67443d22aab3ed67f3190b0b57487
Hitesh Kumar Singh <152964489+Hitesh-singh67@users.noreply.github.com>
2024-01-06 15:02:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var i = 0;
+var txt1 =
+"Hey [Crush's Name]...!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time < You seems something Special to me.  <<                  As the days goes < you get more special to me....! >                        >  I don't know the reason why your thoughts always resonates inside my mind...!                                                   > Everything about you is always intresting for me...!                     << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in future...!                                                     > I Love my Parents so much than anything else in this world....!                    << Now You are the person  whom I like equally with my parents....!                                                                            >So, would you be interested in grabbing a coffee  with me sometime  ...!                                                                                                    > >Thank you";
+
+        
+        var speed = 55;
+typeWriter();
+function typeWriter() {
+  if (i < txt1.length) {
+    if (txt1.charAt(i) == "<")
+      document.getElementById("text1").innerHTML += "</br>";
+    else if (txt1.charAt(i) == ">")
+      document.getElementById("text1").innerHTML = "";
+    else if (txt1.charAt(i) == "|") {
+      $(".bg_heart").css("background-image", "')");
+    } else document.getElementById("text1").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
